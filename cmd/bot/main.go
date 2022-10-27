b3bc96af69896b86240c8d5b47a2bdec936446f4
CitizenF1 <AXpert22@gmail.com>
2022-10-27 20:39:54 +0600

    add logfile

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,17 +1,19 @@
 package main
 
 import (
+	"log"
 	"lunch-manager/internal/bot"
+	"os"
 )
 
 func main() {
 	// ====Route log to file====
-	// f, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	// if err != nil {
-	// 	log.Fatalf("error opening file: %v", err)
-	// }
-	// defer f.Close()
-	// log.SetOutput(f)
+	f, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer f.Close()
+	log.SetOutput(f)
 
 	//Bot Start
 	bot.StartBot()
